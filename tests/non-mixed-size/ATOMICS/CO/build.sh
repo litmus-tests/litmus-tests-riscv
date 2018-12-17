431159ae349fc1ae6850cf1df501d3b175a28b61
Peter Sewell <Peter.Sewell@cl.cam.ac.uk>
2018-12-17 15:46:11 +0000

    RISC-V concurrency litmus tests - moving to public github repo tests written or diy-generated by Shaked Flur and Luc Maranget

---
+++ tests/non-mixed-size/ATOMICS/CO/build.sh
@@ -0,0 +1,10 @@
+mkdir -p TMP
+diy7 -conf X.conf -cond observe -o TMP
+# herd7 -model uniproc.cat TMP/@all > U
+for test in $(ls -1 TMP/*.litmus); do
+  rmem -q -model flat  -eager true -hash_prune true -interactive false "$test" >> U
+  rm -f random_seed.dat
+done
+mlog2cond7 -forall true -optcond true < U > conds.txt
+recond7 -toexists true -conds conds.txt -o . TMP/@all
+rm -rf TMP U conds.txt
